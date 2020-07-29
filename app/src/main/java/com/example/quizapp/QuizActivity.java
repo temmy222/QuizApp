f2b04a8f93321f77f52d776bd6400c877c547f16
Temitope Ajayi <na_me_men@yahoo.com>
2020-07-29 02:04:29 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -0,0 +1,52 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private RadioGroup rdGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private RadioButton rb4;
+    private Button buttonConfirmNext;
+
+    private TextView textViewQuestions;
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
+    private TextView textViewCountDown;
+    private TextView textViewCorrect, textViewWrong;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        setupUI();
+    }
+
+    private void setupUI() {
+        textViewCorrect = findViewById(R.id.txtCorrect);
+        textViewWrong = findViewById(R.id.txtWrong);
+        textViewCountDown = findViewById(R.id.txtTimer);
+        textViewQuestionCount = findViewById(R.id.txtTotalQuestion);
+        textViewScore = findViewById(R.id.txtScore);
+
+        buttonConfirmNext = findViewById(R.id.button);
+        rdGroup = findViewById(R.id.radio_group);
+        rb1 = findViewById(R.id.radio_button1);
+        rb2 = findViewById(R.id.radio_button2);
+        rb3 = findViewById(R.id.radio_button3);
+        rb4 = findViewById(R.id.radio_button4);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/QuizDBHelper.java
@@ -0,0 +1,44 @@
+package com.example.quizapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.quizapp.QuizContract.*;
+
+import androidx.annotation.Nullable;
+
+public class QuizDBHelper extends SQLiteOpenHelper  {
+
+
+    private static final String DATABASE_NAME = "MomoQuiz";
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
+
+    public QuizDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        this.db = db;
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionTable.TABLE_NAME + " ( " +
+                QuestionTable._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionTable.COLUMN_QUESTION + "TEXT, " +
+                QuestionTable.COLUMN_OPTION1 + "TEXT, " +
+                QuestionTable.COLUMN_OPTION2 + "TEXT, " +
+                QuestionTable.COLUMN_OPTION3 + "TEXT, " +
+                QuestionTable.COLUMN_OPTION4 + "TEXT, " +
+                QuestionTable.COLUMN_ANSWER_NR + "INTEGER" +
+                ")";
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+    }
+}
