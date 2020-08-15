797a907e2326b3a07aad6fa3d4fa5a2b4cd8949d
temmy222 <ajayi_temmy@yahoo.com>
2020-08-14 20:34:06 -0500

    Create new activities, transition.xml and some initial code

--- app/src/main/java/com/example/quizapp/PlayActivity.java
+++ app/src/main/java/com/example/quizapp/PlayActivity.java
@@ -2,7 +2,10 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class PlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        Button btPlay = findViewById(R.id.bt_playbutton);
+        btPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PlayActivity.this, QuizActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/quizapp/SplashScreen.java
+++ app/src/main/java/com/example/quizapp/SplashScreen.java
@@ -3,12 +3,20 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    TextView txtSplashText;
+    ImageView imgViewLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        txtSplashText = findViewById(R.id.textviewLogoText);
+        imgViewLogo = findViewById(R.id.imgviewLogo);
     }
 }
